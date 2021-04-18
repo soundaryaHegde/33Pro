03153a2af30d761c4b7ec6ce78614236707a1888
soundaryaHegde <soundarya63@gmail.com>
2021-04-18 22:49:32 +0530

    pRoject33

---
+++ snow.js
@@ -0,0 +1,31 @@
+class snow{
+constructor(){
+
+var options={
+
+    restitutions
+    density
+    friction
+}\
+load the image for snow
+
+craete body
+
+add the boday to the world
+
+
+
+
+
+}
+display(){
+
+diaplay the image
+image(this.image, xposition,yposition,with,height)
+
+}
+
+
+
+
+}
